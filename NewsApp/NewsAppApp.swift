eb74830c5ceaa7559e3d9fe78214f78bc8384281
Raymundo Melecio <raymundo@espoc.com>
2021-07-25 10:22:47 +0800

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by raymundo melecio on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
